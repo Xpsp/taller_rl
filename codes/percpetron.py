3395d27c6a2ff9cb07b80e539a06d5d8b2466c11
Xpsp <juliodltv@gmail.com>
2024-05-21 06:25:27 -0600

    hay va

--- codes/percpetron.py
+++ codes/percpetron.py
@@ -11,24 +11,26 @@ x = np.array([
     [1, 1]
 ])
 
-y = np.array([0, 1, 1, 1]).reshape(-1, 1)
+y = np.array([1, 0, 1, 1])
 
-w = np.random.rand(2).reshape(-1, 1)
+w = np.random.rand(2)
 b = np.random.rand()
 
-# Testing no trained perceptron
-z = np.dot(x, w) + b
-a = sigmoid(z)
-a = np.round(a)
-print("salida del perceptron sin entrenar")
-print(a)
+line = np.linspace(-1, 2, 100)
+plt.plot(line, -(w[0]*line+b)/w[1])
+plt.scatter(x[:, 0], x[:, 1], c=y)
+plt.ylim(-1, 2)
+plt.xlim(-1, 2)
+plt.title("Perceptron sin entrenar")
+plt.show()
+
 
 # Training
 lr = 1.0
 for epoch in range(1000):
     z = np.dot(x, w) + b
     a = sigmoid(z)
-    dL_dz = -(y - a)*a*(1-a)
+    dL_dz = (a-y)*a*(1-a)
     w = w - lr * x.T@dL_dz
     b = b - lr * dL_dz.mean()
 
@@ -38,3 +40,9 @@ a = sigmoid(z)
 a = np.round(a)
 print("\nSalida del perceptron Entrenado")
 print(a)
+
+plt.plot(line, -(w[0]*line+b)/w[1])
+plt.scatter(x[:, 0], x[:, 1], c=y)
+plt.ylim(-1, 2)
+plt.xlim(-1, 2)
+plt.show()
